691d0b5616601aec48a384832e588e9d79ff5a46
mihai1996 <mihailascu13@gmail.com>
2019-04-08 23:25:41 +0300

    Licenta

---
+++ EFitnessMonitoring/EFitnessMonitoring/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFitnessMonitoring.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+    }
+}
---
+++ EFitnessMonitoring/EFitnessMonitoring/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFitnessMonitoring.MvcApplication" Language="C#" %>
